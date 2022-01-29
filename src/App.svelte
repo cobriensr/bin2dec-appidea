bfb60a1d0d3650911cf131b95c514f02a4493e75
cobriensr <charles.a.obrien@outlook.com>
2022-01-29 00:30:49 -0500

    completed MVP - need to work on visuals

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,46 @@
 <script lang="ts">
+import binConvert from './services/binConvert';
+import { bindigit } from './stores/content';
 
+window.onload = () => {
+    const form = document.getElementById('binConvertForm');
+    form?.addEventListener('submit', binConvert)
+};
 </script>
 
 <main>
-
+    <h1>Binary to Decimal Converter</h1>
+    <div id="inputBox">
+        <form id="binConvertForm" name="convert">
+            <label for="bindigit">Enter a Binary Number:</label>
+            <!-- User can enter up to 8 binary digits in one input field -->
+            <!-- User must be notified if anything other than a 0 or 1 was entered -->
+            <input inputmode="numeric" id="bindigit" name="bindigit_input" maxlength="8" minlength="1" placeholder=0 pattern="[0-1]*" required bind:value={$bindigit}>
+        </form>
+    </div>
+    <div id="submitBtn">
+        <button form="binConvertForm" type="submit">Convert!</button>
+    </div>
+    <!-- User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered -->
+    <div id="output">
+    </div>
 </main>
 
 <style>
-
+h1 {
+    text-align: center;
+}
+#inputBox {
+    display: flex;
+    justify-content: center;
+}
+#submitBtn {
+    display: flex;
+    justify-content: center;
+    margin: 1em 1em 1em 1em
+}
+#output {
+    display: flex;
+    justify-content: center;
+}
 </style>
---
+++ src/services/binConvert.ts
@@ -0,0 +1,29 @@
+import { get } from 'svelte/store';
+import { bindigit } from '../stores/content';
+
+// This challenge requires that the developer implementing it follow these constraints:
+// Arrays may not be used to contain the binary digits entered by the user
+// Determining the decimal equivalent of a particular binary digit in the sequence must be calculated using a single mathematical function
+
+export default function binConvert(event: Event) {
+    event.preventDefault();
+    let bindigitValue: string = get(bindigit);
+    let valueLength: number = bindigitValue.length;
+    let result: number = 0;
+    let base: number = 0;
+    // The decimal number is equal to the sum of all the binary numbers with each digit being raised to the power of its index (base 2) before being summed
+    // example: 1111 = 1*(2^3) + 1*(2^2) + 1*(2^1) + 1*(2^0) = 8 + 4 + 2 + 1 = 15
+    for (let i = valueLength-1; i > -1; i--) {
+        let parsedNum: number = Number(bindigitValue.charAt(i));
+        base = Math.pow(2, i);
+        result += (parsedNum*base);
+    }
+    append(result);
+}
+
+function append(result: number) {
+    const output = document.getElementById("output");
+    const p = document.createElement("p");
+    p.textContent = "The converted decimal number is " + result.toString();
+    output?.appendChild(p);
+}
