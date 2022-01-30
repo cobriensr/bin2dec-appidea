d4d760e6298d2b6c8a43b0ef9321105518605d78
cobriensr <charles.a.obrien@outlook.com>
2022-01-30 00:11:38 -0500

    refactored functions and updated CSS for presentation

--- src/App.svelte
+++ src/App.svelte
@@ -9,24 +9,20 @@ window.onload = () => {
 </script>
 
 <main>
-    <h1>Binary to Decimal Converter</h1>
-    <div id="inputBox">
-        <form id="binConvertForm" name="convert">
-            <label for="bindigit">Enter a Binary Number:</label>
+    <h1 class="font-sans font-bold text-center text-2xl subpixel-antialiased">Binary to Decimal Converter</h1>
+    <div id="inputBox" class="flex justify-center">
+        <form id="binConvertForm" name="convert"  class="flex flex-col space-y-3">
+            <label for="bindigit">Binary Number to Convert:</label>
             <!-- User can enter up to 8 binary digits in one input field -->
             <!-- User must be notified if anything other than a 0 or 1 was entered -->
-            <input inputmode="numeric" id="bindigit" name="bindigit_input" maxlength="8" minlength="1" placeholder=0 pattern="[0-1]*" required bind:value={$bindigit}>
-        </form>
-    </div>
-    <div id="submitBtn">
-        <button form="binConvertForm" type="submit">Convert!</button>
-    </div>
-    <!-- User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered -->
-    <div id="output">
-        <form id="unused">
+            <input type="text" inputmode="numeric" id="bindigit" name="bindigit_input" maxlength="8" minlength="1" placeholder=0 pattern="[0-1]*" required bind:value={$bindigit} class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-stone-300">
+            <!-- User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered -->
             <label for="result">Decimal Converted Number:</label>
-            <input type="text" id="result" name="outputBox" placeholder=0>
+            <input type="text" id="result" name="outputBox" placeholder=0 class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-stone-300">
         </form>
+    </div>
+    <div id="submitBtn" class="flex justify-center my-5">
+        <button form="binConvertForm" type="submit" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 bg-stone-300/75 hover:bg-stone-200 active:bg-stone-400 focus:ring-stone-100">Convert!</button>
     </div>
 </main>
 
--- src/services/binConvert.ts
+++ src/services/binConvert.ts
@@ -4,24 +4,23 @@ import { bindigit } from '../stores/content';
 // This challenge requires that the developer implementing it follow these constraints:
 // Arrays may not be used to contain the binary digits entered by the user
 // Determining the decimal equivalent of a particular binary digit in the sequence must be calculated using a single mathematical function
+// The decimal number is equal to the sum of all the binary numbers with each digit being raised to the power of its index (base 2) before being summed
+// example: 1111 = 1*(2^3) + 1*(2^2) + 1*(2^1) + 1*(2^0) = 8 + 4 + 2 + 1 = 15
+
 export default function binConvert(event: Event): void {
-    event.preventDefault();
-    // retrieve value from svelte store object
-    let bindigitValue: string = get(bindigit);
-    // intialize the result variable outside the loop so it can be hoisted
-    let result: number = 0;
-    // The decimal number is equal to the sum of all the binary numbers with each digit being raised to the power of its index (base 2) before being summed
-    // example: 1111 = 1*(2^3) + 1*(2^2) + 1*(2^1) + 1*(2^0) = 8 + 4 + 2 + 1 = 15
-    for (let i: number = bindigitValue.length-1; i > -1; i--) {
-        // retrieve binary number at position i in the binary number string
-        let parsedNum: number = Number(bindigitValue.charAt(i));
-        // raise 2 to the position of i in the binary number string
-        let base: number = Math.pow(2, i);
-        // multiply binary number by the base and sum together to convert to decimal
-        result += (parsedNum*base);
-    }
-    // select the input box to have the output written to
-    const selection = (<HTMLInputElement>document.getElementById('result'));
-    // use dot notation to access value method to change the value of const without changing the binding
+    event.preventDefault()
+    const bindigitValue: string = get(bindigit)
+    const result: number = binStringToNumber(bindigitValue)
+    const selection = document.getElementById('result') as HTMLInputElement
     selection.value = result.toString()
+}
+
+function binStringToNumber(value: string): number {
+    let result: number = 0
+    for (let i: number = value.length - 1; i >= 0; i--) {
+        const parsedNum: number = Number(value.charAt(i))
+        const base: number = Math.pow(2, i)
+        result += parsedNum * base
+    }
+    return result
 }
